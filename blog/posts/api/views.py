959af811fca9a15d4de4060e9e0f111be1341c31
dev-joshua <devjoshuadeguzman@gmail.com>
2018-08-23 17:29:32 +0800

    Initial commit

--- blog/posts/api/views.py
+++ blog/posts/api/views.py
@@ -6,3 +6,44 @@ from rest_framework.response import Response
 class PostListView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def create(self, request, *args, **kwargs):
+        super(PostCreateView, self).create(request, args, kwargs)
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully created",
+                    "result": request.data}
+        return Response(response)
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        super(PostDetailView, self).retrieve(request, args, kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully retrieved",
+                    "result": data}
+        return Response(response)
+
+    def patch(self, request, *args, **kwargs):
+        super(PostDetailView, self).patch(request, args, kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully updated",
+                    "result": data}
+        return Response(response)
+
+    def delete(self, request, *args, **kwargs):
+        super(PostDetailView, self).delete(request, args, kwargs)
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully deleted"}
+        return Response(response)
--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,5 +3,7 @@ from . import views
 from .api import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name=None)
+    path('', views.PostListView.as_view(), name=None),
+    path('create/', views.PostCreateView.as_view(), name=None),
+    path('<int:pk>/', views.PostDetailView.as_view(), name=None)
 ]
